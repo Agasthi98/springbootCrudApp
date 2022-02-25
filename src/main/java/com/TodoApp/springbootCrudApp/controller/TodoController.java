13a955a8712a3af281649377c3e3b94389e4596d
Agasthi998 <agasthi.sankalana@pearson.com>
2022-02-25 10:27:17 +0530

    retrive single data

--- src/main/java/com/TodoApp/springbootCrudApp/controller/TodoController.java
+++ src/main/java/com/TodoApp/springbootCrudApp/controller/TodoController.java
@@ -6,13 +6,11 @@ import com.TodoApp.springbootCrudApp.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TodoController {
@@ -39,6 +37,16 @@ public class TodoController {
         } catch (Exception e) {
             return  new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @GetMapping("/todos/{id}")
+    public ResponseEntity<?> getSingleTodo(@PathVariable("id") String id) {
+       Optional<Todo> todoOptional =  todoRepo.findById(id);
+       if(todoOptional.isPresent()) {
+           return new ResponseEntity<>(todoOptional.get(), HttpStatus.OK);
+       } else {
+           return new ResponseEntity<>("Todo not found with id" +id, HttpStatus.NOT_FOUND);
+       }
     }
 
 }
