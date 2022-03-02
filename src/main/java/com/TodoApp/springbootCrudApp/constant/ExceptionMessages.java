78fbc196afefbfef3950f114bee67c22699b8dc4
Agasthi998 <agasthi.sankalana@pearson.com>
2022-03-02 12:14:47 +0530

    constants updated

--- src/main/java/com/TodoApp/springbootCrudApp/constant/ExceptionMessages.java
+++ src/main/java/com/TodoApp/springbootCrudApp/constant/ExceptionMessages.java
@@ -2,4 +2,5 @@ package com.TodoApp.springbootCrudApp.constant;
 
 public class ExceptionMessages {
     public static final String TODO_DOES_NOT_EXISTS = "Provided Todo-Id does not exists";
+    public static final String TODO_DOES_NOT = "Provided Todo-Id does not exists";
 }
